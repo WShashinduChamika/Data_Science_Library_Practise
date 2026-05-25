a646b80aa7931c14774ec8909079d2eacb80c4ab
WShashinduChamika <shashinduchamika99@gmail.com>
2026-05-25 22:40:00 +0500

    Array indexing for both postive and negative

--- np.py
+++ np.py
@@ -1,25 +1,40 @@
 import numpy as np
 
-print(np.__version__)
+# print(np.__version__)
 
-np_array = np.array([1,2,3,4])
+# np_array = np.array([1,2,3,4])
 
-print(np_array)
-
-
-# Multi dimentional arrays
-
-array_2D = np.array([[1,2,3], [4,5,6]])
-print(array_2D)
-
-array_3D = np.array([[[1,2],[3,4]], [[5,6], [7,8]]])
-print(array_3D)
-
-# Use of ndim for gaining the dimention type
-print(np_array.ndim)
-print(array_2D.ndim)
+# print(np_array)
 
 
-#Use of ndmin for change the dimention as needed
-np_array2 = np.array([1,2,3,4], ndmin=4)
-print(np_array2.ndim)
+# # Multi dimentional arrays
+
+# array_2D = np.array([[1,2,3], [4,5,6]])
+# print(array_2D)
+
+# array_3D = np.array([[[1,2],[3,4]], [[5,6], [7,8]]])
+# print(array_3D)
+
+# # Use of ndim for gaining the dimention type
+# print(np_array.ndim)
+# print(array_2D.ndim)
+
+
+# #Use of ndmin for change the dimention as needed
+# np_array2 = np.array([1,2,3,4], ndmin=4)
+# print(np_array2.ndim)
+
+# Array indexing
+arr = np.array([1,2,3])
+print(arr[1])
+
+# Multi dimenational array indexing
+arr_2D = np.array([[1,2,3], [5,6,7], [7,8,9]])
+print(arr_2D[0,1])
+
+arr_3D =  np.array([[[1,2],[2,3]],[[4,5],[6,7]]])
+print(arr_3D[0,1,0])
+
+# Negative indexing
+new_arr = np.array([1,2,3,4,5])
+print(new_arr[-3])
